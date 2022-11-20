12f78cd962009960589ff2e4af9bb45f8e6aad12
HyunSoo Kim <118273341+rising-developer@users.noreply.github.com>
2022-11-20 09:35:43 +0900

    [Bronze I] Title: 약수, Time: 124 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[0] * arr[arr.length - 1]);
+		br.close();
+	}
+} 
