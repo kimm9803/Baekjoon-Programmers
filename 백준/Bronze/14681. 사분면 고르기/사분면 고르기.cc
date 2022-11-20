e84b498f96d3555e05ea374dd1cb113358608ad7
HyunSoo Kim <118273341+rising-developer@users.noreply.github.com>
2022-11-20 13:33:44 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cin >> x;
+	cin >> y;
+
+	if (x > 0 && y > 0) {
+		cout << "1" << endl;
+	}
+	else if (x < 0 && y > 0) {
+		cout << "2" << endl;
+	}
+	else if (x < 0 && y < 0) {
+		cout << "3" << endl;
+	}
+	else {
+		cout << "4" << endl;
+	}
+	return 0;
+}
