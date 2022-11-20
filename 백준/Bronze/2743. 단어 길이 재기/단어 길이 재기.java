67528b5bee198a94de17b3712eab650ee53954bb
HyunSoo Kim <118273341+rising-developer@users.noreply.github.com>
2022-11-20 10:10:20 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        
+        System.out.println(str.length());
+    }
+}
