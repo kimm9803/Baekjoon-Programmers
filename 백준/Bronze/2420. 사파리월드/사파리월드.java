225148f4233b65967add88dd0282940ea1ea790a
HyunSoo Kim <118273341+rising-developer@users.noreply.github.com>
2022-12-03 23:05:47 +0900

    [Bronze V] Title: 사파리월드, Time: 120 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+
+        System.out.println(Math.abs(n-m));
+    }
+}
