f91405a316682a88699038318445bd6b75ff684c
HyunSoo Kim <118273341+rising-developer@users.noreply.github.com>
2023-01-17 22:42:42 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for (int i = 1; i*i <= n; i++) {
+            if (i*i == n) {
+                answer = 1;
+            } else {
+                answer = 2;
+            }
+        }
+        return answer;
+    }
+}
