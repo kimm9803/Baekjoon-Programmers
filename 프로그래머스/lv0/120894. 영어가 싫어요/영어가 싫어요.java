d8c36b6c7de9e78b2e66b00abd8b3d491483529e
HyunSoo Kim <118273341+rising-developer@users.noreply.github.com>
2023-02-03 12:38:16 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.09 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        String switchNumbers = numbers
+            .replace("zero", "0")
+            .replace("one", "1")
+            .replace("two", "2")
+            .replace("three", "3")
+            .replace("four", "4")
+            .replace("five", "5")
+            .replace("six", "6")
+            .replace("seven", "7")
+            .replace("eight", "8")
+            .replace("nine", "9");
+        
+        answer = Long.parseLong(switchNumbers);
+        return answer;
+    }
+}
