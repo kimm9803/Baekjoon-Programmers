8a9c56533a9674f91ca661bc61d0cfc7e0eb0483
HyunSoo Kim <118273341+rising-developer@users.noreply.github.com>
2023-02-08 12:15:51 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if (num == 0) {
+            answer = "Even";
+        } else if (num % 2 == 0) {
+            answer = "Even";
+        } else {
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
