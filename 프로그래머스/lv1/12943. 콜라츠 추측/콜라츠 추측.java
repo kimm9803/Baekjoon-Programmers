f13fdbe8f7313f9164998a371b4a59171fd87eb6
HyunSoo Kim <118273341+rising-developer@users.noreply.github.com>
2023-02-10 12:47:16 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        while (num != 1) {
+            if (answer == 500) {
+                answer = -1;
+                break;
+            }
+            if (num % 2 == 0) {
+                num /= 2;
+            } else if (num % 2 == 1) {
+                num = num * 3 + 1;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
