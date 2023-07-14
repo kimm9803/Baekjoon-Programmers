35d4f3cc36feec16b7d161e0036ae6e9c8700373
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2023-07-14 10:24:40 +0900

    [unrated] Title: 0 떼기, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int before = Integer.parseInt(n_str);   // "0010" -> 10
+        answer = String.valueOf(before);
+        return answer;
+    }
+}
