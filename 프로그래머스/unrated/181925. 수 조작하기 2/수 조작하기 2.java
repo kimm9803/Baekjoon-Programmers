e03a13e440876d5e2fb16e639264b666eccc7658
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2023-07-19 15:58:30 +0900

    [unrated] Title: 수 조작하기 2, Time: 599.25 ms, Memory: 379 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        for (int i = 0; i < numLog.length-1; i++) {
+            if (numLog[i+1] == numLog[i] + 1) {
+                answer += 'w';
+            } else if (numLog[i+1] == numLog[i] - 1) {
+                answer += 's';
+            } else if (numLog[i+1] == numLog[i] + 10) {
+                answer += 'd';
+            } else if (numLog[i+1] == numLog[i] - 10) {
+                answer += 'a';
+            }
+        }
+        return answer;
+    }
+}
