4873f5ee2cef5849e2aa9b27bcf87d3e208ad55a
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2023-07-25 17:52:24 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 124 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st;
+		while (true) {
+			st = new StringTokenizer(br.readLine(), " ");
+			String name = st.nextToken();
+			int age = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			
+			if (name.equals("#") && age == 0 && weight == 0) {
+				break;
+			}
+			
+			if (age > 17 || weight >= 80) {
+				System.out.println(name + " Senior");
+			} else {
+				System.out.println(name + " Junior");
+			}
+		}
+		
+	}
+}
