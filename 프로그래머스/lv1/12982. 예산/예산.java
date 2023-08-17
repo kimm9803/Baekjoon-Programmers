63f6a7342eb5ffa2d798304e5001512809fba51e
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2023-08-17 09:57:10 +0900

    [level 1] Title: 예산, Time: 0.41 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+        Arrays.sort(d);
+        for (int i = 0; i < d.length; i++) {
+            sum += d[i];
+            if (sum <= budget)   answer++;
+            else                break;
+        }
+        return answer;
+    }
+}
