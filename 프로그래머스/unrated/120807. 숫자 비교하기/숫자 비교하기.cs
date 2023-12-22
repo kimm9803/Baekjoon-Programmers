6365deb11451477f311426353e245e4d788f253a
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2023-12-22 16:52:28 +0900

    [level unrated] Title: 숫자 비교하기, Time: 0.15 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120807. 숫자 비교하기/숫자 비교하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if (num1 == num2) {
+            answer = 1;
+        } else {
+            answer = -1;
+        }
+        return answer;
+    }
+}
