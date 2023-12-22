d61be68986d9d7c87bf04458bce2706350fd1804
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2023-12-22 17:01:08 +0900

    [level unrated] Title: 두 수의 나눗셈, Time: 0.21 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = (int)((double)num1 / (double)num2 * 1000);
+        return answer;
+    }
+}
