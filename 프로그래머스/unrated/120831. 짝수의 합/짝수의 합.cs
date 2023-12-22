bd745931624dfa239a4768297cdd1c3a16185434
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2023-12-22 17:08:36 +0900

    [level unrated] Title: 짝수의 합, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
