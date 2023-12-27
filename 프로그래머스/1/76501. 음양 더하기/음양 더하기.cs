14210b05f09656877ee946ed76872cb434c0a379
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2023-12-27 23:38:09 +0900

    [level 1] Title: 음양 더하기, Time: 0.18 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int[] absolutes, bool[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.Length; i++) {
+            if (signs[i] == true) {
+                answer += absolutes[i];
+            } else {
+                answer += absolutes[i] * -1;
+            }
+        }
+        return answer;
+    }
+}
