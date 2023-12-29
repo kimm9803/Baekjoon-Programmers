263ced8ec5304c9cdc4b4a071fcf4bf51a4e62bd
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2023-12-29 18:33:53 +0900

    [Bronze V] Title: 아스키 코드, Time: 60 ms, Memory: 5448 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Algorithm_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = Console.ReadLine();
+            int asc = (int)str[0];
+            Console.WriteLine(asc);
+        }
+    }
+}
+
