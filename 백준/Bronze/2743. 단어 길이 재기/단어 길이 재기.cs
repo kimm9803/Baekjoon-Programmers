5eeabaaf8c7c48c845d18ddb6bad6a9eb03565ee
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2023-12-29 18:21:57 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 64 ms, Memory: 5452 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Algorithm_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(str.Length);
+        }
+    }
+}
+
