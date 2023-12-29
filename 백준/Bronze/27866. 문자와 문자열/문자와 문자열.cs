6f8879c26d782a0eeafe6661bf851bd15f65837e
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2023-12-29 18:19:57 +0900

    [Bronze V] Title: 문자와 문자열, Time: 60 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Algorithm_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine(str[i-1]);
+        }
+    }
+}
+
