22e12f4a99ff5c11666bdea86e80a20dc4365ea8
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2023-12-29 22:42:51 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 64 ms, Memory: 5384 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Algorithm_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            while ((str = Console.ReadLine()) != null) {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
+
