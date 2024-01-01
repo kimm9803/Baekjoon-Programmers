43bc4c8e3ae3077d42d3ffd140d880bc4b668b00
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-01-01 13:56:43 +0900

    [level 2] Title: 가장 큰 수, Time: 3.42 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cs
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cs
@@ -3,23 +3,19 @@ using System;
 public class Solution {
     public string solution(int[] numbers) {
         string answer = "";
-
+        
         string[] nums = new string[numbers.Length];
         for (int i = 0; i < numbers.Length; i++) {
             nums[i] = numbers[i].ToString();
         }
-
         Array.Sort(nums, CompareNumbers);
-
-        answer = string.Concat(nums);
-
-        if (answer[0] == '0')
-            return "0";
         
+        answer = string.Concat(nums);
+        if (answer[0] == '0') return "0";
         return answer;
     }
-
-    private int CompareNumbers(string a, string b) {
+    
+    public int CompareNumbers(string a, string b) {
         string ab = a + b;
         string ba = b + a;
         return ba.CompareTo(ab);
