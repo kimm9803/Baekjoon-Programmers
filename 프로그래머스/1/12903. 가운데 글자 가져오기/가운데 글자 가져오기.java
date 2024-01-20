b0947a4ff4a95d9649f62b1569859296ecfc480d
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-01-21 01:52:16 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 9.75 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int index = s.length() / 2;
+        if (s.length() % 2 == 1) {
+            answer += s.charAt(index);
+        } else {
+            answer += s.charAt(index - 1);
+            answer += s.charAt(index);
+        }
+        return answer;
+    }
+}
