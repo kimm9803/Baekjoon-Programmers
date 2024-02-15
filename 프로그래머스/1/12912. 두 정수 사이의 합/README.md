79d05fca76f98878818944de2d09818dddf635c8
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-02-15 19:32:30 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.17 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/README.md
+++ 프로그래머스/1/12912. 두 정수 사이의 합/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 정수 사이의 합 - 12912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912) 
 
 ### 성능 요약
 
-메모리: 31.4 MB, 시간: 0.23 ms
+메모리: 31.4 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 3일 23:30:13
+2024년 2월 4일 19:32:27
 
 ### 문제 설명
 
--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
@@ -1,12 +1,13 @@
 public class Solution {
     public long solution(int a, int b) {
         long answer = 0;
-        if (a <= b) {
-            for (long i = a; i <= b; i++) {
+        
+        if (a >= b) {
+            for (int i = b; i <= a; i++) {
                 answer += i;
             }
         } else {
-            for (long i = b; i <= a; i++) {
+            for (int i = a; i <= b; i++) {
                 answer += i;
             }
         }
