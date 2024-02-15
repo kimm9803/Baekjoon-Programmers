2f3e8ee926fc38474411bccf753459d3b631e40e
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-02-15 20:02:52 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/README.md
+++ 프로그래머스/1/12943. 콜라츠 추측/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라츠 추측 - 12943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943) 
 
 ### 성능 요약
 
-메모리: 70.7 MB, 시간: 0.02 ms
+메모리: 73.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 23:3:21
+2024년 2월 4일 20:2:50
 
 ### 문제 설명
 
--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -3,23 +3,18 @@ class Solution {
         int answer = 0;
         
         while (true) {
-            // 주어진 수가 1이 되면 반복문 종료
-            if (num == 1) {
-                break;
-            }
-            
+            if (num == 1)   break;
             if (answer == 500) {
                 answer = -1;
                 break;
             }
             
             if (num % 2 == 0) {
-                // 짝수라면 2로 나눔
                 num /= 2;
             } else if (num % 2 == 1) {
-                // 홀수라면 3을 곱하고 1을 더함
-                num = num * 3 + 1;
+                num = (num * 3) + 1;
             }
+            
             answer++;
         }
         
