3bace3095e3ebbfb05e247b88b21c80c071fd55c
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-02-18 17:53:19 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 26.70 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i-1]) {
+                list.add(arr[i]);
+            }
+        }
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
