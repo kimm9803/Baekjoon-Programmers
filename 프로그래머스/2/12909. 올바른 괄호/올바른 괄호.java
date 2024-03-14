aae7b1a4a7a34a16feb9c2e96fc928d6dad024f9
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-03-14 11:57:37 +0900

    [level 2] Title: 올바른 괄호, Time: 19.60 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+            } else if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        
+        answer = stack.isEmpty();
+        
+        return answer;
+    }
+}
