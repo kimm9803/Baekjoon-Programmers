734494304f62418bad5a30d80310e75f71cf77a9
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-08-12 23:16:43 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = quotient(num1, num2);
+        return answer;
+    }
+    
+    public int quotient(int num1, int num2) {
+        int quo = num1 / num2;
+        return quo;
+    }
+}
