8d9e0f16b45550b6adc2f2255f09f6b9da06a37e
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-08-12 23:16:36 +0900

    [level 0] Title: 두 수의 곱, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = multiplication(num1, num2);
+        return answer;
+    }
+    
+    public int multiplication(int num1, int num2) {
+        int mul = num1 * num2;
+        return mul;
+    }
+}
