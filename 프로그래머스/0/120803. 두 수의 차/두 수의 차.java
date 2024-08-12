97034fc82c13c4c178e61409b8def13a1012736a
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-08-12 23:18:32 +0900

    [level 0] Title: 두 수의 차, Time: 0.04 ms, Memory: 65.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = substraction(num1, num2);
+        return answer;
+    }
+    
+    public int substraction(int num1, int num2) {
+        return num1 - num2;
+    }
+}
