6252780045ec449a00fd4e150738277d6a1cb709
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-08-19 13:06:40 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for (int i : array) {
+            if (i > height) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
