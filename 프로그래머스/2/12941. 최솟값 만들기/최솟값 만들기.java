8a48c774ab8c3773d61576e6993b7dd9408d7f96
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-09-14 18:04:14 +0900

    [level 2] Title: 최솟값 만들기, Time: 6.39 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -6,17 +6,25 @@ class Solution
     public int solution(int []A, int []B)
     {
         int answer = 0;
+        Integer[] integerB = new Integer[B.length];
+        
+        for (int i =0; i < B.length; i++) {
+            integerB[i] = B[i];
+        }
+        
         Arrays.sort(A);
-        
-        Integer[] IntegerB = new Integer[B.length];
-        for (int i = 0; i < B.length; i++) {
-            IntegerB[i] = B[i];
-        }
-        Arrays.sort(IntegerB, Collections.reverseOrder());
-        
+        Arrays.sort(integerB, Collections.reverseOrder());
+
         for (int i = 0; i < A.length; i++) {
-            answer += A[i] * IntegerB[i];
+            answer += A[i] * integerB[i];
         }
+        
         return answer;
     }
 }
+
+/*
+ *  두 개의 배열에서 한 개의 숫자를 뽑아 곱하고 누적을 반복하여 최솟값 만들기
+ *  각 배열의 최소와 최대를 곱하여 더하면 최솟값이 됨
+ *  A 오름차순, B 내림차순 정렬
+*/
