3db20e24ff927bd433af76672d895c876d82dd90
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-09-14 17:50:40 +0900

    [level 2] Title: 올바른 괄호, Time: 18.00 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -15,9 +15,16 @@ class Solution {
                 stack.pop();
             }
         }
-
         answer = stack.isEmpty();
-        
         return answer;
     }
 }
+
+/*
+    스택을 이용한 알고리즘
+    반복문을 통해 문자열 탐색
+    '(' 가 나오면 스택에 담음
+    ')' 가 나오면 스택 빼냄
+    최종적으로 스택이 비어있으면 true / else false
+    else 스택이 비어있는 상태에서 ')' 가 나오면 false
+*/
