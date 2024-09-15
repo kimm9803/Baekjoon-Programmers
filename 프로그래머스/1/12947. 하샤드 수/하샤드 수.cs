d0e2cb0387e2f9838d801927893ab94e887d463c
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-09-15 16:09:26 +0900

    [level 1] Title: 하샤드 수, Time: 0.27 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
@@ -1,15 +1,15 @@
 public class Solution {
     public bool solution(int x) {
         bool answer = true;
+        int sum = 0;
         int copyX = x;
         
-        int sum = 0;
-        while (x > 0) {
-            sum += x % 10;
-            x /= 10;
+        while (copyX > 0) {
+            sum += copyX % 10;
+            copyX /= 10;
         }
         
-        if (copyX % sum == 0) {
+        if (x % sum == 0) {
             answer = true;
         } else {
             answer = false;
@@ -18,3 +18,10 @@ public class Solution {
         return answer;
     }
 }
+
+/*
+ * 하샤드 수
+ * x의 자릿수의 합을 구하려면 while문을 통해 x가 0이 될 때까지 반복하여 합을 구함.
+ * 단, 값을 복사하여 사용
+ * 나누어 떨어지면 true, 아니면 false
+*/
