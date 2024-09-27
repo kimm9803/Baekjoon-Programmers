acf55756a06429ecd65e8ed1ff350e17423eaace
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-09-27 23:42:34 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,15 +1,26 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
+        boolean[] arr = new boolean[10];
+        Arrays.fill(arr, false);
         
-        int[] chk = new int[10];
         for (int i = 0; i < numbers.length; i++) {
-            chk[numbers[i]] = 1;
+            arr[numbers[i]] = true;
         }
         
-        for (int i = 0; i < 10; i++) {
-            if (chk[i] == 0)    answer += i;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == false) {
+                answer += i;
+            }
         }
         return answer;
     }
 }
+
+/*
+ *  1. 10의 크기를 가진 배열 선언
+ *  2. numbers 배열을 돌면서 각 원소값에 해당하는 인덱스를 true
+ *  3. false인 인덱스를 더하여 return
+*/
