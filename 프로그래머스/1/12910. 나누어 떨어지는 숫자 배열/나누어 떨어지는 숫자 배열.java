e5fc0eb3189dc1d53795794a9be969bea7c6d886
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-09-28 13:30:02 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 1.56 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -4,25 +4,34 @@ import java.util.Collections;
 
 class Solution {
     public int[] solution(int[] arr, int divisor) {
+        int[] answer;
         List<Integer> list = new ArrayList<>();
         
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % divisor == 0) {
-                list.add(arr[i]);
+        for (int num : arr) {
+            if (num % divisor == 0) {
+                list.add(num);
             }
         }
-        
-        Collections.sort(list);
-        int[] answer;
-        
+
         if (list.size() == 0) {
-            answer = new int[] { -1 };
+            list.add(-1);
         } else {
-            answer = new int[list.size()];
-            for (int j = 0; j < list.size(); j++) {
-                answer[j] = list.get(j);
-            }
+            Collections.sort(list);
         }
+        
+        answer = new int[list.size()];
+        
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
         return answer;
     }
 }
+
+/*
+ *  1. List 선언
+ *  2. arr 배열을 돌면서 divisor로 나누어떨어지는 수를 List에 add
+ *  3. 이 때, List가 비어있다면 -1 담은 후 리턴
+ *  4. List 오름차순 정렬 후 answer 배열에 담아 리턴
+*/
