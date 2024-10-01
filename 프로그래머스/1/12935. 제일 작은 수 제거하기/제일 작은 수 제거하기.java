8001ef604d6ab5b0d0dea69da4cc67a4710d9455
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-10-01 18:56:03 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 5.46 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,41 +1,36 @@
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr) {
         int[] answer;
+        List<Integer> list = new ArrayList<>();
         
         if (arr.length == 1) {
             answer = new int[1];
             answer[0] = -1;
+        } else {
+            answer = new int[arr.length - 1];
+            int min = arr[0];
+            int minIndex = 0;
             
-            return answer;
-        }
-        
-        answer = new int[arr.length - 1];
-        
-        int min = 2147483647;
-        int index = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-                index = i;
+            for (int i = 0; i < arr.length; i++) {
+                if (min > arr[i]) {
+                    min = arr[i];
+                    minIndex = i;
+                }
+            }
+            
+            for (int i = 0; i < arr.length; i++) {
+                list.add(arr[i]);
+            }
+            
+            list.remove(minIndex);
+            
+            for (int i = 0; i < list.size(); i++) {
+                answer[i] = list.get(i);
             }
         }
-        
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            list.add(arr[i]);
-        }
-        
-        list.remove(index);
-        
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        
-        
         return answer;
     }
 }
