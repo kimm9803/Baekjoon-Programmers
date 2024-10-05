4921ffc0ad40f02bb023980cd97360bea43b6a2a
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-10-05 18:12:58 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 43.51 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -4,10 +4,15 @@ class Solution {
         for (int i = 1; i <= n; i++) {
             if (i % 2 == 1) {
                 answer += "수";
-            } else {
+            } else if (i % 2 == 0) {
                 answer += "박";
             }
         }
         return answer;
     }
 }
+
+/*
+ *  for문 n까지 반복하면서 i가 홀수면 "수" 추가,
+ *  짝수면 "박" 추가
+*/
