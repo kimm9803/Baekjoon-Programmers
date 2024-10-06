94ef9b0adb9c861d16c540f750d65ecc1645d6b7
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-10-06 18:29:09 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,27 +1,16 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
-        
+        boolean answer = false;
         if (s.length() == 4 || s.length() == 6) {
-            answer = true;
-        } else {
-            answer = false;
-            return answer;
-        }
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '0' || s.charAt(i) == '1' || s.charAt(i) == '2' ||
-                s.charAt(i) == '3' || s.charAt(i) == '4' || s.charAt(i) == '5' ||
-                s.charAt(i) == '6' || s.charAt(i) == '7' || s.charAt(i) == '8' || 
-                s.charAt(i) == '9') {
-                
-                answer = true;
-            } else {
-                answer = false;
-                break;
+            for (int i = 0; i < s.length(); i++) {
+                if (Character.isDigit(s.charAt(i))) {
+                    answer = true;
+                    continue;
+                } else {
+                    return false;
+                }
             }
         }
-        
         return answer;
     }
 }
