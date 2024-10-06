93fb8bf7d77bcb45ff9abece015fc201407eaa3f
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-10-06 18:35:07 +0900

    [level 1] Title: 행렬의 덧셈, Time: 19.54 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -3,7 +3,7 @@ class Solution {
         int[][] answer = new int[arr1.length][arr1[0].length];
         
         for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr1[i].length; j++) {
+            for (int j = 0; j < arr1[0].length; j++) {
                 answer[i][j] = arr1[i][j] + arr2[i][j];
             }
         }
