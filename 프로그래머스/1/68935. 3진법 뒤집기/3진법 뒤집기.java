ecb7ba864fff2315ad934c68e34e8be0393aa6a5
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-10-15 23:38:02 +0900

    [level 1] Title: 3진법 뒤집기, Time: 14.07 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,15 +1,19 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        // 3진법
+        
+        // 10진법 -> 3진법
         String trinaryDigit = Integer.toString(n, 3);
-        // 3진법(앞뒤 반전)
+        
+        // 앞뒤 반전(3진법)
         String reverseTrinaryDigit = "";
         for (int i = trinaryDigit.length() - 1; i >= 0; i--) {
             reverseTrinaryDigit += trinaryDigit.charAt(i);
         }
-        // 10진법
+        
+        // 3진법 -> 10진법
         answer = Integer.parseInt(reverseTrinaryDigit, 3);
+        
         return answer;
     }
 }
