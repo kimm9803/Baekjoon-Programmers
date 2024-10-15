b813200390ecdd7fb583fc6cc4dcb5caf3aaf24b
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-10-15 13:58:51 +0900

    [level 1] Title: 예산, Time: 0.52 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/README.md
+++ 프로그래머스/1/12982. 예산/README.md
@@ -1,10 +1,10 @@
 # [level 1] 예산 - 12982 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982) 
 
 ### 성능 요약
 
-메모리: 72.9 MB, 시간: 0.33 ms
+메모리: 66.6 MB, 시간: 0.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 0일 18:30:57
+2024년 10월 15일 13:58:49
 
 ### 문제 설명
 
--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -3,10 +3,8 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int sum = 0;
-        
         Arrays.sort(d);
-        
+        int sum = 0;
         for (int i = 0; i < d.length; i++) {
             sum += d[i];
             if (sum > budget) {
@@ -18,3 +16,9 @@ class Solution {
         return answer;
     }
 }
+
+/*
+ *  각 부서가 신청한 금액 배열을 오름차순 정렬
+ *  for문을 이용하여 누적합을 구하다가 예산(budget)을 넘어가면
+ *  for문 종료
+*/
