1220a7046e62dcd605298d519dcebd7e5cb6e36c
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-10-16 15:00:54 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 20.89 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,22 +1,35 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] str = s.split("");
-        int index = 0;
         
-        for (int i = 0; i < str.length; i++) {
-            if (str[i].equals(" ")) {
-                index = 0;
-            } else if (index % 2 == 0) {
-                str[i] = str[i].toUpperCase();
-                index++;
-            } else if (index % 2 == 1) {
-                str[i] = str[i].toLowerCase();
-                index++;
+        String[] arr = s.split(" ", -1);
+        for (int i = 0; i < arr.length; i++) {
+            String str = "";
+            for (int j = 0; j < arr[i].length(); j++) {
+                if (j % 2 == 0) {
+                    str += Character.toUpperCase(arr[i].charAt(j));
+                } else if (j % 2 == 1) {
+                    str += Character.toLowerCase(arr[i].charAt(j));
+                }
             }
-            answer += str[i];
+            arr[i] = str;
         }
         
+        String str2 = "";
+        for (int i = 0; i < arr.length; i++) {
+            str2 += arr[i] + " ";
+        }
+        
+        for (int i = 0; i < str2.length() - 1; i++) {
+            answer += str2.charAt(i);
+        }
         return answer;
     }
 }
+
+/*
+ *  각 단어를 공백 기준으로 나눠 배열 생성
+ *  0, 2, ... 등 짝수번째는 대문자로
+ *  1, 3, ... 등 홀수번째는 소문자로
+ *  문자열 앞에 공백이 오거나 끝에 공백이 오는 반례 고민
+*/
