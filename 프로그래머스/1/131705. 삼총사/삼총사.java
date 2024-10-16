7210366e1bee16aacab60a31752f5053dbdc9710
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-10-16 15:23:56 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -2,8 +2,8 @@ class Solution {
     public int solution(int[] number) {
         int answer = 0;
         for (int i = 0; i < number.length - 2; i++) {
-            for (int j = i+1; j < number.length - 1; j++) {
-                for (int k = j+1; k < number.length; k++) {
+            for (int j = i + 1; j < number.length - 1; j++) {
+                for (int k = j + 1; k < number.length; k++) {
                     if (number[i] + number[j] + number[k] == 0) {
                         answer++;
                     }
@@ -13,3 +13,9 @@ class Solution {
         return answer;
     }
 }
+
+/*
+ *  브루트포스 알고리즘
+ *  모든 경우의 수를 구하여 값을 구함
+ *  3중 for문 예상
+*/
