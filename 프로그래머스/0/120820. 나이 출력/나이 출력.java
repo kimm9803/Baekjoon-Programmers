cb40d2fc87519769e287275f8e3bd6b953f3d58a
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-12-16 00:09:42 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/0/120820. 나이 출력/나이 출력.java
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int age) {
-        int answer = calYear(age);    
-        
+        int answer = getYear(age);
         return answer;
     }
     
-    public int calYear(int age) {
-        int year = 2022;
-        int tYear = year - age + 1;
+    public int getYear(int age) {
+        int result = 2022 - age + 1;
         
-        return tYear;
+        return result;
     }
 }
