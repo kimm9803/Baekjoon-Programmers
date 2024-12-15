171390495be35570b1f6ff57124b882dcbced542
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2024-12-16 00:04:27 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,17 +1,18 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
-        
-        answer = compare(num1, num2);
-        
+        int answer = compareNum(num1, num2);
         return answer;
     }
     
-    public int compare(int a, int b) {
-        if (a == b) {
-            return 1;
+    public int compareNum(int num1, int num2) {
+        int result = 0;
+        
+        if (num1 == num2) {
+            result = 1;
         } else {
-            return -1;
+            result = -1;
         }
+        
+        return result;
     }
 }
