2abd3c0e78475326ac2a6a7442a140c7dbded718
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2025-09-14 23:46:25 +0900

    [Bronze V] Title: Hello World, Time: 100 ms, Memory: 13944 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,13 @@
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write("Hello World!");
+		bw.flush();
+		bw.close();
+	}
 }
