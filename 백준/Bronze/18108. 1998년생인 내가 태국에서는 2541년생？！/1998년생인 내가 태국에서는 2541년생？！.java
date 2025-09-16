36119fdb0be3ac1fdca444a092dad4770a802fb6
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2025-09-17 00:42:50 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int year = Integer.parseInt(br.readLine());
+		
+		System.out.println(year - 543);
+	}
+}
