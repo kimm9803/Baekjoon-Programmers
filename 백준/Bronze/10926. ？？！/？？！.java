9f69db5d74a9c459c993ab9529b8060634d6eefd
HyunSoo Kim <118273341+kimm9803@users.noreply.github.com>
2025-09-17 00:38:27 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        System.out.println(str + "??!");
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		System.out.println(str + "??!");
+	}
 }
-
